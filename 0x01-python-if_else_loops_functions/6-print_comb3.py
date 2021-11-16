d09d18d182355cb4e3ab3e17d005093d1f18028e
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-16 18:21:43 +0000

    fixed bug on task 0,1,2,3,5,6,7,11 and 12

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(10):
         if i != j and i < j:
             if i == 8 and j == 9:
                 print("{}{}".format(i,j))
+                continue
             print("{}{}".format(i,j),end=", ")
