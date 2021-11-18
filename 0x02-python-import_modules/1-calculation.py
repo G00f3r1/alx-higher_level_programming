d293710b3624a722d2cbd4d71be272b5102cfb94
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-18 09:56:41 +0000

    bug fixed on task 1 and 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,6 @@ a = 10
 b = 5
 
 print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, cal.sub(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, cal.mul(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, cal.div(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ import sys
 
 arg_len = len(sys.argv) - 1
 
-if (arg_len == 1):
-    print("{:d} argument".format(arg_len))
+if (arg_len == 0):
+    print("{:d} arguments.".format(arg_len))
+elif (arg_len == 1):
+    print("{:d} argument:".format(arg_len))
 else:
-    print("{:d} arguments".format(arg_len))
+    print("{:d} arguments:".format(arg_len))
 
 i = 1
 while(arg_len >= i):
