00a76ffff13c530f83e1227021849a9267362782
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-22 10:41:55 +0000

    fixing task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,11 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
             row_len = len(row)
+            i = 1
             for item in row:
-                if row_len > 1:
-                    print("{:d}".format(item), end=" ")
-                else:
+                if row_len == i:
                     print("{:d}".format(item), end="")
+                else:
+                    print("{:d}".format(item), end=" ")
+                i += 1
             print()
