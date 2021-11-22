361839d9fc95ea1846d55708c6dd4cf482fdb88a
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-22 09:39:49 +0000

    fixing bug on task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,10 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+
     tuple_a_len = len(tuple_a)
     tuple_b_len = len(tuple_b)
-    
+
     if tuple_a_len < 2:
         if tuple_a_len == 1:
             new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
