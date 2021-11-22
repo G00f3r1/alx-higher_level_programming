e68ad72de6873898a1b060f5ebfbf0f2c57e1973
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-22 09:36:24 +0000

    fixing bug on task 3, 6 and 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        row_len = len(row)
-        for item in row:
-            if row_len > 1:
-                print("{:d}".format(item), end=" ")
-            else:
-                print("{:d}".format(item), end="")
-        print()
+    if matrix:
+        for row in matrix:
+            row_len = len(row)
+            for item in row:
+                if row_len > 1:
+                    print("{:d}".format(item), end=" ")
+                else:
+                    print("{:d}".format(item), end="")
+            print()
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,20 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_b) < 1:
-        default = (0, 0)
-        new_tuple = tuple(map(lambda i, j: i + j, tuple_a, default))
-    elif len(tuple_b) < 2:
-        default = (tuple_b[0], 0)
-        new_tuple = tuple(map(lambda i, j: i + j, tuple_a, default))
+    
+    tuple_a_len = len(tuple_a)
+    tuple_b_len = len(tuple_b)
+    
+    if tuple_a_len < 2:
+        if tuple_a_len == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+        else:
+            new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
+    elif tuple_b_len < 2:
+        if tuple_b_len == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+        else:
+            new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
     else:
-        new_tuple = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return new_tuple
