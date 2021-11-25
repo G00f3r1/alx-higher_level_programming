292ff58fccc5e13bf4ac7c235ac6ef2ef85f8480
Amanuel Adamu <amanueladamu18@gmail.com>
2021-11-25 09:53:25 +0000

    fixing bug on task 1 and 6

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,11 +5,13 @@ def search_replace(my_list, search, replace):
     if my_list:
         list_len = len(my_list)
         i = 0
-        new_list = []
-        while(list_len > i):
-            if my_list[i] == search:
-                new_list.append(replace)
-            else:
-                new_list.append(my_list[i])
-            i += 1
-    return new_list
+        if list_len >= search:
+            new_list = []
+            while(list_len > i):
+                if my_list[i] == search:
+                    new_list.append(replace)
+                else:
+                    new_list.append(my_list[i])
+                i += 1
+            return new_list
+    return my_list
