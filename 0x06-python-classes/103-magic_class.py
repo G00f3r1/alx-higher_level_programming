309d9c8da7179a7afa36d3063147dc016536e0c4
Amanuel Adamu <amanueladamu18@gmail.com>
2021-12-08 01:50:38 +0000

    fixing task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,19 @@ import math
 """
 
 class MagicClass:
+    """MagicClass
+    """
     def __init__(self, radius=0):
+        """__init__
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+            radius (int): The size of the square.
+
+        Raises:
+            TypeError: If `radius` type is not `number`.
+
+        """
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
