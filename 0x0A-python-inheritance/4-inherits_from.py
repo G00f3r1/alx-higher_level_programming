75ff54d44464d2d0198ea1471b55504c8e28f4d9
Amanuel Adamu <amanueladamu18@gmail.com>
2021-12-21 00:21:10 +0000

    fixing task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """This function cheks if an object is an inherited instance of a class"""
-    if (isinstance(obj, a_class) and
+    if (isinstance(type(obj), a_class) and
             isinstance(a_class, obj.__class__) is False):
         return True
     else:
