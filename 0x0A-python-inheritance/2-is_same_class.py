a9c80ea396c53ff67060beeff81cb75bfb910455
Amanuel Adamu <amanueladamu18@gmail.com>
2021-12-21 00:09:50 +0000

    fixing task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,7 +9,7 @@ def is_same_class(obj, a_class):
             obj(any): the object to be cheked
             a_class(type): the type to be cheked with
     """
-    if type(obj) is a_class:
+    if type(obj) == a_class:
         return True
     else:
         False
