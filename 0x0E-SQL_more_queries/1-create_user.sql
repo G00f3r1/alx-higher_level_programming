8afd6751eafbbe950fede6e54595117d0ac9524e
Amanuel Adamu <amanueladamu18@gmail.com>
2022-02-03 04:41:46 +0000

    fixing task 2

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,8 +2,7 @@
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
+GRANT SELECT
    ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
