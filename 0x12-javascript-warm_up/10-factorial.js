40b12ecbb05cdc2c67c04339dcdcccec4dfda37d
Amanuel Adamu <amanueladamu18@gmail.com>
2022-02-28 12:57:37 +0000

    fixing task 2 and 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 const args = process.argv;
 
 function factorial (n) {
-  if (n == 0 || isNaN(n)) {
+  if (n === 0 || isNaN(n)) {
     return 1;
   }
-  return n * factorial(n-1)
+  return n * factorial(n - 1);
 }
 
 if (parseInt(args[2])) {
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const args = process.argv;
 
-if (args.length >= 3) {
+if (args.length === 3) {
   console.log('Argument found');
+} else if (args.length >= 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
