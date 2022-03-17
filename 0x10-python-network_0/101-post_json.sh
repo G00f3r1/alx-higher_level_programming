1b05df1f4dbf5db004287248d1d62b83d1b44f58
Amanuel Adamu <amanueladamu18@gmail.com>
2022-03-17 22:16:58 +0000

    fixing task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument, and displays the body
-curl -sXHd POST "content-type: application/json" @"$2" "$1"
+curl -sX POST -H "content-type: application/json" -d @"$2" "$1"
